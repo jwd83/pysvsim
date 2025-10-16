db702a76394763176038151e3291641da8410b90
Jared De Blander <jared0x90@gmail.com>
2025-10-16 05:07:36 -0400

    need to fix reduction operators. 015-gates4.sv from my coursework passes on the original solution i had but not when using reduction operators.

--- coursework/011-VerilogLanguage-Vectors/015-Gates4.sv
+++ coursework/011-VerilogLanguage-Vectors/015-Gates4.sv
@@ -6,14 +6,14 @@ module top_module(
 );
 
     // original solution
-    // assign out_and = in[0] & in[1] & in[2] & in[3];
-    // assign out_or = in[0] | in[1] | in[2] | in[3];
-    // assign out_xor = in[0] ^ in[1] ^ in[2] ^ in[3];
+    assign out_and = in[0] & in[1] & in[2] & in[3];
+    assign out_or = in[0] | in[1] | in[2] | in[3];
+    assign out_xor = in[0] ^ in[1] ^ in[2] ^ in[3];
 
     // alternative solution with reduction operators
-    assign out_and = &in;
-    assign out_or = |in;
-    assign out_xor = ^in;
+    // assign out_and = &in;
+    // assign out_or = |in;
+    // assign out_xor = ^in;
 
 
 endmodule
