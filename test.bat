9cd28e04fb2c494cf0e8773092f5c11c4f2b2bd6
Jared De Blander <jared0x90@gmail.com>
2026-02-11 08:23:15 -0500

    updates

--- test.bat
+++ test.bat
@@ -1,2 +1,3 @@
 uv run test_runner.py testing\ > test_results_testing.txt
 uv run test_runner.py parts\ > test_results_parts.txt
+uv run test_runner.py parts\overture\ > test_results_parts_overture.txt
